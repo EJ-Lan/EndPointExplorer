f851fcee6a4c0b6e10ecd34bdcab0e72200981ad
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-04-06 00:54:06 -0600

    Make the menu

--- EndPointExplorer/Program.cs
+++ EndPointExplorer/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EndPointExplorer.Utilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set initial console properties
+        Console.Title = "API Testing Tool";
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+
+        bool running = true;
+        while (running)
+        {
+            ConsoleUtility.DisplayMenu();
+            var choice = ConsoleUtility.ReadInput("Select an option: ");
+            switch (choice)
+            {
+                case "1":
+                    // Handle GET request
+                    // You can call a method here that performs the GET operation
+                    ConsoleUtility.PrintSuccess("GET request executed.");
+                    break;
+                case "2":
+                    // Handle POST request
+                    // Similar to the GET, call a method for the POST operation
+                    ConsoleUtility.PrintSuccess("POST request executed.");
+                    break;
+                case "3":
+                    // Handle PUT request
+                    ConsoleUtility.PrintSuccess("PUT request executed.");
+                    break;
+                case "4":
+                    // Handle DELETE request
+                    ConsoleUtility.PrintSuccess("DELETE request executed.");
+                    break;
+                case "5":
+                    // Exit the application
+                    running = false;
+                    ConsoleUtility.PrintSuccess("Exiting application. Goodbye!");
+                    break;
+                default:
+                    ConsoleUtility.PrintError("Invalid option, please try again.");
+                    break;
+            }
+
+            if (running)
+            {
+                ConsoleUtility.ReadInput("Press Enter to continue..."); // Pause before clearing the screen
+                Console.Clear(); // Clear the console for a fresh start
+            }
+        }
+    }
+
+}
---
+++ EndPointExplorer/Utilities/ConsoleUtility.cs
@@ -0,0 +1,55 @@
+﻿namespace EndPointExplorer.Utilities
+{
+    internal class ConsoleUtility
+    {
+        public static void PrintSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static string ReadInput(string prompt)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(prompt);
+            var input = Console.ReadLine();
+            Console.ResetColor();
+            return input;
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.Clear(); // Clear the console to keep it neat
+
+            Console.WriteLine(@" _____  ______  _____ ");
+            Console.WriteLine(@"|  ___|| ___ \\|  ___|");
+            Console.WriteLine(@"| |__  | |_/ /| |__  ");
+            Console.WriteLine(@"|  __| |  __/ |  __| ");
+            Console.WriteLine(@"| |___ | |    | |___ ");
+            Console.WriteLine(@"\\____/ \\_|    \\____/ ");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the API Testing Tool");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+            // Menu options
+            Console.ForegroundColor = ConsoleColor.Green; // Use a different color for options
+            Console.WriteLine("1. Send GET request");
+            Console.WriteLine("2. Send POST request");
+            Console.WriteLine("3. Send PUT request");
+            Console.WriteLine("4. Send DELETE request");
+            Console.WriteLine("5. Exit");
+            Console.ResetColor(); // Reset to default colors
+            Console.Write("\nSelect an option: ");
+        }
+    }
+
+}
