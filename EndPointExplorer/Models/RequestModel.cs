84bfd4bf7d9d728a040bb5851430cd98a323af4b
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-04-06 00:58:40 -0600

    RequestModel

---
+++ EndPointExplorer/Models/RequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EndPointExplorer.Models
+{
+    internal class RequestModel
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+        public string Body { get; set; }
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>;
+    }
+}
